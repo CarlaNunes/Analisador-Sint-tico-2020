81007905599a826076ad3a30ccbb83bf215546e9
MarileneGarcia <marilenegarcia1998@gmail.com>
2020-04-15 17:27:56 -0300

    automato identificadores

--- main.cpp
+++ main.cpp
@@ -70,6 +70,37 @@ class Punctuation {
         bool AutomatoSimDif(string punctuation);          
 };
 
+class Words {
+    private:
+        string program = "program";
+        string begin = "begin";
+        string end = "end";
+        string var = "var";
+        string ident_else = "else";
+        string read = "read";
+        string write = "write";
+        string ident_if = "if";
+        string then = "then";
+        string ident_while = "while";
+        string ident_do = "do";
+        string ident_for = "for";
+        string to = "to";
+        string procedure = "procedure";
+        string integer = "integer";
+        string real = "real";
+
+
+    public:
+        int asc_begin0 = 65;
+        int asc_end0 = 90;
+
+        int asc_begin1 = 97;
+        int asc_end1 = 122;
+
+        int asc_begin2 = 48;
+        int asc_end2 = 57;
+        bool AutomatoIdent(string number); 
+};
 
 
 int main(int argc, char *argv[]) 
@@ -172,6 +203,9 @@ void token(string word)
     Punctuation pont;
     bool flag_pont;
 
+    Words ident;
+    bool flag_ident;
+
 
     /* Descobrindo se o numero asc da primeira letra da palavra é um numero
         e caso for, chama o automato que checa se é inteiro ou real*/
@@ -184,7 +218,7 @@ void token(string word)
             flag_num = num.AutomatoNumReal(word);
 
         if(flag_num == false)
-            cout << "erro_dig  " << word << endl;
+            name_token = "erro(número não permitido)";
     }
 
     /* Descobrindo se o numero asc da primeira letra da palavra é uma pontuacao
@@ -225,13 +259,17 @@ void token(string word)
         if(flag_pont == false)
             flag_pont = pont.AutomatoSimDif(word); 
         if(flag_pont == false)
-            cout << "erro_dig  " << word << endl;
+            name_token = "erro(simbolo não permitido)";
     }   
 
-    else
-    {
-        name_token = "id";
+    else if (ident.asc_begin0 <= int (*word.begin()) &&  ident.asc_end0 >= int (*word.begin()) || ident.asc_begin1 <= int (*word.begin()) &&  ident.asc_end1 >= int (*word.begin() )){   
+        flag_ident = ident.AutomatoIdent(word);
+        if(flag_ident == false)
+            name_token = "erro(identificador não permitido)";
     }
+
+    else 
+        name_token = "erro(caractere não permitido)";
     
     cout << "Token " << count_tokens << ": " << word << ", " << name_token << endl;
 
@@ -404,3 +442,78 @@ bool Punctuation::AutomatoSimDif(string punctuation){
     }
     return false;
 }
+
+bool Words::AutomatoIdent(string ident){
+    string::iterator it_ident;
+    for(it_ident = ident.begin(); it_ident != ident.end(); ++it_ident){
+        if( asc_begin2 > *it_ident || (asc_end2 < *it_ident && asc_begin0 > *it_ident) || asc_end1 < *it_ident || (asc_end0 < *it_ident && asc_begin1 > *it_ident) )
+            return false;    
+    }
+    if (ident.length() == program.length() && program.compare(ident) == 0){
+        name_token = "simb_program";
+        return true;
+    }
+    else if(ident.length() == end.length() && end.compare(ident) == 0){
+        name_token = "simb_end";
+        return true;
+    }
+    else if(ident.length() == begin.length() && begin.compare(ident) == 0){
+        name_token = "simb_begin";
+        return true;
+    }
+    else if(ident.length() == var.length() && var.compare(ident) == 0){
+        name_token = "simb_var";
+        return true;
+    }
+    else if(ident.length() == ident_else.length() && ident_else.compare(ident) == 0){
+        name_token = "simb_else";
+        return true;
+    }
+    else if(ident.length() == read.length() && read.compare(ident) == 0){
+        name_token = "simb_read";
+        return true;
+    }
+    else if(ident.length() == write.length() && write.compare(ident) == 0){
+        name_token = "simb_write";
+        return true;
+    }
+    else if(ident.length() == ident_if.length() && ident_if.compare(ident) == 0){
+        name_token = "simb_if";
+        return true;
+    }
+    else if(ident.length() == then.length() && then.compare(ident) == 0){
+        name_token = "simb_then";
+        return true;
+    }
+    else if(ident.length() == ident_while.length() && ident_while.compare(ident) == 0){
+        name_token = "simb_while";
+        return true;
+    }
+    else if(ident.length() == ident_do.length() && ident_do.compare(ident) == 0){
+        name_token = "simb_do";
+        return true;
+    }
+    else if(ident.length() == ident_for.length() && ident_for.compare(ident) == 0){
+        name_token = "simb_for";
+        return true;
+    }
+    else if(ident.length() == to.length() && to.compare(ident) == 0){
+        name_token = "simb_to";
+        return true;
+    }
+    else if(ident.length() == procedure.length() && procedure.compare(ident) == 0){
+        name_token = "simb_procedure";
+        return true;
+    }
+    else if(ident.length() == integer.length() && integer.compare(ident) == 0){
+        name_token = "simb_integer";
+        return true;
+    }
+    else if(ident.length() == real.length() && real.compare(ident) == 0){
+        name_token = "simb_real";
+        return true;
+    }
+    name_token = "id";
+    return true;
+}
+        
